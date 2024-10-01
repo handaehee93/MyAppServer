35e710cb0d0850522ffad26666ed79a9cc2640e1
handaehee93 <hdh8990@naver.com>
2024-10-01 19:34:42 +0900

    Add MyAppServerLogin folder

---
+++ MyAppServerLogin/src/main/java/com/MyApp/MyAppServerLogin/MyAppServerLoginApplication.java
@@ -0,0 +1,13 @@
+package com.MyApp.MyAppServerLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppServerLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppServerLoginApplication.class, args);
+	}
+
+}
---
+++ MyAppServerLogin/src/test/java/com/MyApp/MyAppServerLogin/MyAppServerLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyApp.MyAppServerLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppServerLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
